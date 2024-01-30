eca758fa24d4e49b63e1bc637a77125e9e66974a
Emad1615 <76161087+Emad1615@users.noreply.github.com>
2024-01-30 15:21:31 +0200

    fullScreenCommit

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,33 @@
-import { FaChalkboard } from 'react-icons/fa';
+import { BsArrowsFullscreen } from 'react-icons/bs';
+import { IoIosNotifications } from 'react-icons/io';
+
 function Header() {
   return (
     <ul className="header fixed top-0 z-10 mb-16 flex w-full flex-row items-center   justify-between gap-10 pb-1 pl-10 pr-10 pt-1 text-white shadow-lg">
-      <li>
-        <span className="loader"></span>
+      <li className="flex flex-row items-center justify-start gap-2">
+        <span className="loader"></span>{' '}
+        <span className="font-semibold">TASKS BOARD</span>
       </li>
-      <li>
-        <h4 className=" text-xl font-semibold  text-white">
-          <FaChalkboard className="inline-block text-2xl" /> &nbsp;TASKS BOARD
-        </h4>
+      <li className=" flex flex-row items-center justify-start gap-3">
+        <button className="Notifications relative">
+          <IoIosNotifications
+            title="Notifications"
+            className="   cursor-pointer text-2xl"
+          />
+        </button>
+        <button
+          onClick={() => {
+            if (!document.fullscreenElement) {
+              document.documentElement.requestFullscreen();
+            } else {
+              if (document.exitFullscreen) {
+                document.exitFullscreen();
+              }
+            }
+          }}
+        >
+          <BsArrowsFullscreen title="Full screen" className="cursor-pointer" />
+        </button>
       </li>
     </ul>
   );
